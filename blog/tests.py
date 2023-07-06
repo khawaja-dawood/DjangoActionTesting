653f77c6e508ad0bebb818977da411ce40c4de22
Khawaja Dawood <dawoodnaveed0@gmail.com>
2023-07-06 19:15:25 +0500

    updated git workflow and add some test

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from blog.models import Post
 
-# Create your tests here.
+
+class ModelTesting(TestCase):
+    def setUp(self):
+        self.post = Post.objects.create(
+            title='django',
+            author='John',
+            slug='django'
+        )
+
+    def test_post_model(self):
+        d = self.post
+        self.assertTrue(isinstance(d, Post))
+        self.assertEqual(str(d), 'django')
